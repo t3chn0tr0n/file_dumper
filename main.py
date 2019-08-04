e1fd6bc46f71ced887020b3e722a2e1ff700f458
t3chn0tr0n <technotron.avik@gmail.com>
2019-08-04 11:32:20 +0530

    add schedular

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-import shutil
+from os import walk
+from shutil import move
+import time
+from schedule import every, run_pending
 
 '''
 STEPS:
@@ -10,9 +12,17 @@ STEPS:
 '''
 
 
+def get_source_sink():
+    source = "./dummy_data/source"
+    sink = "./dummy_data/sink"
+    # source = input("Enter Source Directory: ").strip()
+    # sink = input("Enter Sink Directory: ").strip()
+    return source, sink
+
+
 def get_list_of_files(directory="."):
     try:
-        for root, dirs, files in os.walk(directory):
+        for root, dirs, files in walk(directory):
             pass
         return files
     except:
@@ -23,28 +33,40 @@ def move_file(file_name="", source_dir="", dest_dir=""):
     try:
         source_file_path = source_dir + '/' + file_name
         dest_file_path = dest_dir + '/' + file_name
-        shutil.move(source_file_path, dest_file_path)
+        move(source_file_path, dest_file_path)
         return True
     except:
         print("ERROR")
         return False
 
 
-def get_source_sink():
-    source = "D:/DOCUMENTS/Programs/Python/source"
-    sink = "D:/DOCUMENTS/Programs/Python/sink"
-    # source = input("Enter Source Directory: ").strip()
-    # sink = input("Enter Sink Directory: ").strip()
-    return source, sink
+def operation():
+    source, sink = get_source_sink()
+    list_of_files = get_list_of_files(source)
+    counter = 0
+    if list_of_files:
+        for file in list_of_files:
+            if not move_file(file, source, sink):
+                print("ERROR: File Cannot be moved!")
+            else:
+                counter += 1
+        if counter:
+            print("Successfully Moved {} files".format(counter))
+        else:
+            print("NO FILES MOVED")
+    elif list_of_files == []:
+        print("INFO: No New File Found!")
+    else:
+        print("ERROR: Source NOT FOUND!")
 
 
-source, sink = get_source_sink()
-list_of_files = get_list_of_files(source)
-if list_of_files:
-    for file in list_of_files:
-        if not move_file(file, source, sink):
-            print("ERROR: File Cannot be moved!")
-elif list_of_files == []:
-    print("INFO: No New File Found!")
-else:
-    print("ERROR: Source NOT FOUND!")
+def operate():
+    every(10).seconds.do(operation)
+    while 1:
+        try:
+            run_pending()
+        except:
+            print("INTERNAL ERROR")
+
+
+operate()
